6f3b4a297ce4e6b0c27093ee83a05db73b59a726
André Carvalho <andre.vrlcarvalho@hotmail.com>
2022-06-01 20:51:01 -0300

    s4

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'https://teste-ex-escobar.herokuapp.com/';
+$config['base_url'] = 'teste-ex-escobar.herokuapp.com';
 
 /*
 |--------------------------------------------------------------------------
